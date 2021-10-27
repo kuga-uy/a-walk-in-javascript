5507135fef582dbc75463f87eb9babaac5966a8d
kuga-uy <eduardodiaz250196@gmail.com>
2021-10-27 13:13:13 -0300

    exception handling completed

--- src/day_05/exceptionHandling.test.js
+++ src/day_05/exceptionHandling.test.js
@@ -1,15 +1,29 @@
 describe('DAY 5: Test Exception Handling', () => {
 
     it('throw explicitly an error and on catch define a as true', () => {
-        let a;
+        let a = false;
 
+        try {
+           throw 'error';
+            
+        } catch (error) {
+            a = true;
+        }
         expect(a).toBe(true);
 
     });
 
     it('throw explicitly an error and assign the error to a, make the verification of the message', () => {
-        let a;
+        let a; 
+        
+        try {
+            throw "Error: I'm an error";
+        } catch (error) {
+            a = error;
+        }
+        
 
+        
         expect(a.toString()).toBe(`Error: I'm an error`);
 
     });
@@ -17,6 +31,11 @@ describe('DAY 5: Test Exception Handling', () => {
     it('throw explicitly an error and assign the error to a, make the verification of type', () => {
         let a;
 
+        try {
+            throw new Error("Error: I'm an error");
+        } catch (error) {
+            a = error;
+        }
         expect(a).toBeInstanceOf(Error);
 
     });
